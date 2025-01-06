91c6e95e6d89b7bfc45b909b52acd697b1fd64b9
AlbiXD <albizhaku15@gmail.com>
2025-01-05 22:25:12 -0500

    Patched some bugs somewhat functional

--- Chip8.cpp
+++ Chip8.cpp
@@ -10,12 +10,12 @@ struct Chip8
 	uint8_t mem[4096]; // Memory is 4096 bytes, from location 0x000 to 0xFFF
 	// The first 512 bytes, from 0x000 to 0x1FF are reserved;
 
-	uint8_t V[16];              // 16 Registers with 8-bit (0-F in hexadecimal)
-	uint16_t I;                 // Registor to store memory addresses
-	uint16_t PC;                // Stores currently executing address
-	uint8_t SP;                 // Stack pointer (Points at the top of the stack)
-	uint16_t stack[16];         // Stack is an array of 16 16-bit values
-	uint8_t gfx[64 * 32];   // Created a buffer for the graphics 64 by 
+	uint8_t V[16];		  // 16 Registers with 8-bit (0-F in hexadecimal)
+	uint16_t I;			  // Registor to store memory addresses
+	uint16_t PC;		  // Stores currently executing address
+	uint8_t SP;			  // Stack pointer (Points at the top of the stack)
+	uint16_t stack[16];	  // Stack is an array of 16 16-bit values
+	uint8_t gfx[64 * 32]; // Created a buffer for the graphics 64 by
 	bool draw_flag = false;
 	bool clearScreen_flag = false;
 	uint8_t keys[16];
@@ -34,7 +34,7 @@ struct Chip8
 	/*
 		TODO: Load instructions into memory
 	*/
-	bool loadROM(const char* file_path)
+	bool loadROM(const char *file_path)
 	{
 		std::ifstream file(file_path, std::ios::binary);
 
@@ -44,9 +44,9 @@ struct Chip8
 			return false;
 		}
 
-		file.seekg(0, std::ios::end);           // Moves pointer to the end of the file
+		file.seekg(0, std::ios::end);			// Moves pointer to the end of the file
 		std::streampos fileSize = file.tellg(); // Gets the byte position at the end
-		file.seekg(0, std::ios::beg);           // Moves the pointer back to the beginning of the file in order to transfer bytes into memory
+		file.seekg(0, std::ios::beg);			// Moves the pointer back to the beginning of the file in order to transfer bytes into memory
 
 		if (fileSize > (4096 - 512)) // Checks if file is too large
 		{
@@ -54,7 +54,7 @@ struct Chip8
 			return false;
 		}
 
-		if (!file.read(reinterpret_cast<char*>(mem + 512), fileSize))
+		if (!file.read(reinterpret_cast<char *>(mem + 512), fileSize))
 		{ // Casts mem (int8_t*) to a char* and stores its values
 			std::cerr << "Failed to read ROM into memory." << std::endl;
 			return false;
@@ -72,281 +72,332 @@ struct Chip8
 		byte1 = mem[PC];
 		byte2 = mem[PC + 1];
 
-		opcode = (byte1 << 8) | byte2; //COMBINE OPCODE IN MEMORY
-		std::cout << "OPCODE : " << std::hex << opcode << " ";
+		opcode = (byte1 << 8) | byte2; // COMBINE OPCODE IN MEMORY
+		std::cout << "OPCODE : " << std::hex << opcode << " Address: " << std::dec << PC << " ";
 
-		PC += 2; //INCREMENT PC BY 2
+		PC += 2; // INCREMENT PC BY 2
 
 		// DECODE AND EXECUTE
 		uint8_t instruction = opcode >> 12; // FIRST NIBBLE
 
 		switch (instruction)
 		{
-		case(0x0): //0x0 INSTRUCTIONS
+		case (0x0): // 0x0 INSTRUCTIONS
 		{
-			if ((opcode) == 0xE0) { //CLEAR SCREEN
+			if ((opcode) == 0xE0)
+			{ // CLEAR SCREEN
 				std::fill(std::begin(gfx), std::end(gfx), 0);
 				std::cout << "CLEAR SCREEN CALLED" << std::endl;
 				clearScreen_flag = true;
 				break;
 			}
-			if ((opcode) == 0x00EE) { //RETURN FROM SUBROUTINE
+			if ((opcode) == 0x00EE)
+			{ // RETURN FROM SUBROUTINE
 				std::cout << "RETURN" << std::endl;
 				PC = this->pop();
 				std::cout << "THIS IS CURRENT PC " << PC << std::endl;
 			}
-			else {
+			else
+			{
 				std::cout << "UNKNOWN OPCODE " << std::hex << opcode << std::endl;
 			}
 			break;
 		}
-		case(0x1): { //1nnn - JUMP INSTRUCTION
+		case (0x1):
+		{ // 1nnn - JUMP INSTRUCTION
 			uint16_t address = opcode & 0x0FFF;
 			PC = address;
-			std::cout << "JUMP INSTRUCTION TO ADDRESS " << (int) PC << std::endl;
+			std::cout << "JUMP INSTRUCTION TO ADDRESS " << (int)PC << std::endl;
 			break;
 		}
-		case(0x2): { //2nnn - CALLING SUBROUTINE AT ADDRESS nnn
+		case (0x2):
+		{ // 2nnn - CALLING SUBROUTINE AT ADDRESS nnn
 			uint16_t address = opcode & 0x0FFF;
-			std::cout << "CALLING SUBROUTINE TO " << address << std::endl;
+			std::cout << "CALLING SUBROUTINE TO " << std::dec << address << std::endl;
 			this->push(PC);
 			PC = address;
 			break;
 		}
-		case (0x3): { // 3xkk - SKIP INSTRUCTION IF Vx == kk
+		case (0x3): //3xkk
+		{									   // 3xkk - SKIP INSTRUCTION IF Vx == kk
 			uint8_t vx = (opcode >> 8) & 0x0F; // EXTRACT REGISTER NUMBER
-			uint8_t byte = opcode & 0xFF;      // EXTRACT BYTE VALUE
-
-			if (V[vx] == byte) {
+			uint8_t byte = opcode & 0xFF;	   // EXTRACT BYTE VALUE
+			if (V[vx] == byte)
+			{
 				PC += 2; // Skip the next instruction
+				std::cout << "3xkk SKIP INSTRUCTION IF Vx == kk" << std::endl;
+
 			}
-			std::cout << "3xkk SKIP INSTRUCTION IF Vx == kk" << std::endl;
+			std::cout << "3xkk SKIP INSTRUCTION IF Vx == kk (FAILED)" << std::endl;
 			break;
 		}
 
-		case(0x4): { //4xkk SKIP INSTRUCTION IF Vx != kkk
+		case (0x4):
+		{									   // 4xkk SKIP INSTRUCTION IF Vx != kkk
 			uint8_t vx = (opcode >> 8) & 0x0F; // EXTRACT REGISTER NUMBER
-			uint8_t byte = opcode & 0xFF;      // EXTRACT BYTE VALUE
+			uint8_t byte = opcode & 0xFF;	   // EXTRACT BYTE VALUE
 
-			if (V[vx] != byte) {
+			if (V[vx] != byte)
+			{
 				PC += 2; // Skip the next instruction
 			}
+
 
 			std::cout << "4xkk SKIP INSTRUCTION IF Vx != kk" << std::endl;
 			break;
 		}
 
-		case(0x5): ////5xy0 SKIP INSTRUCTION IF Vx == Vy
+		case (0x5): ////5xy0 SKIP INSTRUCTION IF Vx == Vy
 		{
 
 			uint8_t vx = (opcode >> 8) & 0x0F; // EXTRACT REGISTER NUMBER
 			uint8_t vy = (opcode >> 4) & 0x0F; // EXTRACT REGISTER NUMBER
-			if (!((opcode & 0x000F) == 0)) {
+			if (!((opcode & 0x000F) == 0))
+			{
 				std::cout << "INVALID OPCODE FORMAT" << std::endl;
 				break;
 			}
-			if (V[vx] == V[vy]) {
-				PC += 2;
+			if (V[vx] == V[vy])
+			{
+				std::cout << "5xy0 SKIP INSTRUCTION IF Vx == Vy" << std::endl;
+				PC += 2; // Skips the next instruction
 				break;
 			}
+			std::cout << "Test" << std::endl;
 			break;
 		}
 		case (0x6): // LD Vx , BYTE (SET Vx TO BYTE)
 		{
 			uint8_t vx = (opcode >> 8) & 0x0F; // EXTRACT REGISTER NUMBER
-			uint8_t byte = opcode & 0xFF;      // EXTRACT BYTE VALUE
-			V[vx] = byte;                      //SETS THE Vx REGISTER TO BYTE
+			uint8_t byte = opcode & 0xFF;	   // EXTRACT BYTE VALUE
+			V[vx] = byte;					   // SETS THE Vx REGISTER TO BYTE
 			std::cout << "LD INSTRUCTION" << std::endl;
 			break;
 		}
-		case(0x7): {//7xkk ADD Vx, BYTE
+		case (0x7):
+		{									   // 7xkk ADD Vx, BYTE
 			uint8_t vx = (opcode >> 8) & 0x0F; // EXTRACT REGISTER NUMBER
-			uint8_t byte = opcode & 0xFF;      // EXTRACT BYTE VALUE
-			V[vx] = (V[vx] + byte) & 0xFF; // ENSURE WRAPPING IN ORDER TO AVOID GOING OVER 255
+			uint8_t byte = opcode & 0xFF;	   // EXTRACT BYTE VALUE
+			V[vx] = (V[vx] + byte) & 0xFF;	   // ENSURE WRAPPING IN ORDER TO AVOID GOING OVER 255
 
 			std::cout << "ADD INSTRUCTION V" << (int)vx << ", " << (int)byte << std::endl;
 
 			break;
 		}
-		case(0x8):
+		case (0x8):
 		{
 			uint8_t nibble = (opcode & 0x0F);
 			uint8_t x = (opcode >> 8) & 0x0F; // EXTRACT REGISTER NUMBER
 			uint8_t y = (opcode >> 4) & 0x0F; // EXTRACT REGISTER NUMBER
+			std::cout << "------------------+ Case 8 +---------------------" << std::endl;
 
-			switch (nibble) {
-				case(0x00): {
-					V[x] = V[y];
-					std::cout << "LD Vx, Vy" << std::endl;
-					break;
-				}
-				case(0x01): {
-					V[x] = V[x] | V[y];
-					std::cout << "OR Vx, Vy" << std::endl;
-					break;
-				}
-				case(0x02): {
-					V[x] = V[x] & V[y];
-					std::cout << "AND Vx, Vy" << std::endl;
-					break;
-				}
-				case(0x03): {
-					V[x] = V[x] ^ V[y];
-					std::cout << "XOR Vx, Vy "  << std::endl;
-					break;
-				}
-				case(0x04): {
-					uint16_t result = V[x] + V[y];
-					V[0xF] = (result > 255) ? 0x01 : 0x00;
-					V[x] = result & 0xFF;
-					std::cout << "ADD Vx, Vy" << std::endl;
-					break;
-				}
-				case(0x05): {
-					V[0x0F] = (V[x] > V[y]) ? 0x01 : 0x00;
-					V[x] -= V[y];
-					std::cout << "SUB Vx, Vy" << std::endl;
-					break;
-				}
-				case(0x06): {
-					uint8_t lsb = V[x] & 1;
+			switch (nibble)
+			{
+			case (0x00):
+			{
+				V[x] = V[y];
+				std::cout << "LD Vx, Vy" << std::endl;
+				break;
+			}
+			case (0x01):
+			{
+				V[x] = V[x] | V[y];
+				std::cout << "OR Vx, Vy" << std::endl;
+				break;
+			}
+			case (0x02):
+			{
+				V[x] = V[x] & V[y];
+				std::cout << "AND Vx, Vy" << std::endl;
+				break;
+			}
+			case (0x03):
+			{
+				V[x] = V[x] ^ V[y];
+				std::cout << "XOR Vx, Vy " << std::endl;
+				break;
+			}
+			case (0x04):
+			{ // 8xy4
+				uint16_t result = V[x] + V[y];
+				V[0x0F] = (result > 0xFF) ? 0x01 : 0x00;
 
-					V[0x0F] = (lsb == 1) ? 0x01 : 0x00;
-					V[x] >>= 1;
+				V[x] = result & 0xFF;
+				std::cout << "ADD Vx, Vy" << std::endl;
+				break;
+			}
+			case (0x05):
+			{
+				std::cout << (V[x] > V[y]) << std::endl;
+				V[0x0F] = (V[x] >= V[y]) ? 0x01 : 0x00;
+				V[x] -= V[y];
+				std::cout << "SUB Vx, Vy" << std::endl;
+				break;
+			}
+			case (0x06):
+			{
+				uint8_t lsb = V[x] & 1;
 
-					std::cout << "SHR Vx {, Vy}" << std::endl;
-					break;
-				}
-				case(0x07): {
-					V[0x0F] = (V[y] > V[x]) ? 0x01 : 0x00;
-					V[x] = V[y] - V[x];
-					std::cout << "SUBN Vx, Vy" << std::endl;
-					break;
-				}
-				case(0x0E): {
-					uint8_t msb = V[x] & 0x80;
+				V[0x0F] = (lsb == 1) ? 0x01 : 0x00;
+				V[x] >>= 1;
 
-					V[0x0F] = (msb == 0x80) ? 0x01 : 0x00;
-					V[x] <<= 1;
+				std::cout << "SHR Vx {, Vy}" << std::endl;
+				break;
+			}
+			case (0x07):
+			{
+				V[0x0F] = (V[y] >= V[x]) ? 0x01 : 0x00;
+				V[x] = V[y] - V[x];
+				std::cout << "SUBN Vx, Vy" << std::endl;
+				break;
+			}
+			case (0x0E):
+			{
 
-					std::cout << "SHL Vx {, Vy}" << std::endl;
-					break;
-				}
+				V[0xF] = (V[x] >> 7) & 0x1;
+				V[x] <<= 1;
+
+				std::cout << "SHL Vx {, Vy}" << std::endl;
+				break;
+			}
 			}
 			break;
 		}
-		case(0x9): {
-			if ((opcode & 0x00F) != 0x0) {
+		case (0x9):
+		{
+			if ((opcode & 0x00F) != 0x0)
+			{
 				std::cout << "INVALID OPCODE FORMAT" << std::endl;
 				break;
 
-			//0009 Vx00 Vy00 0000
+				// 0009 Vx00 Vy00 0000
 			}
 			uint8_t x = (opcode >> 8) & 0x0F; // EXTRACT REGISTER NUMBER
 			uint8_t y = (opcode >> 4) & 0x0F; // EXTRACT REGISTER NUMBER
 
-			if (V[x] != V[y]) {
+			if (V[x] != V[y])
+			{
 				PC += 2;
 			}
 			break;
 		}
-		case(0xA): { //Annn SET VALUE OF I TO ADDRESS
+		case (0xA):
+		{ // Annn SET VALUE OF I TO ADDRESS
 			uint16_t address = opcode & 0x0FFF;
 			I = address;
 			std::cout << "Register I is set to " << I << std::endl;
 			break;
 		}
-		case(0xB): {
+		case (0xB):
+		{
 
 			PC = ((opcode) & 0x0FFF) + V[0];
 			std::cout << "JP V0, addr" << std::endl;
 			break;
 		}
-		case(0xC): {
+		case (0xC):
+		{
 			uint8_t x = (opcode & 0xF00) >> 8;
-			uint8_t kk = opcode & 0x00FF;       
+			uint8_t kk = opcode & 0x00FF;
 
 			V[x] = generateRandomByte() & kk;
 			std::cout << "RND Vx, byte" << std::endl;
 
 			break;
 		}
-		case(0xD): //Dxyn - DRAW Vx, Vy, HEIGHT
+		case (0xD): // Dxyn - DRAW Vx, Vy, HEIGHT
 		{
 			uint8_t x = V[(opcode >> 8) & 0x0F]; // X-COORDINATE FROM VX (WRAPPING TO PREVENT FURTHER THAN 64PX)
 			uint8_t y = V[(opcode >> 4) & 0x0F]; // Y-COORDINATE FROM VY (WRAPPING TO PREVENT FURTHER THAN 32PX)
-			uint8_t height = opcode & 0x0F;      // HEIGHT OF SPRITE
+			uint8_t height = opcode & 0x0F;		 // HEIGHT OF SPRITE
 			V[0xF] = 0;							 // Reset collision flag
 
-			for (int row = 0; row < height; row++) {
+			for (int row = 0; row < height; row++)
+			{
 				uint8_t sprite_byte = mem[I + row];
-				for (int col = 0; col < 8; col++) {
-					if ((sprite_byte & (0x80 >> col)) != 0) {
+				for (int col = 0; col < 8; col++)
+				{
+					if ((sprite_byte & (0x80 >> col)) != 0)
+					{
 						int gfx_index = ((y + row) % 32) * 64 + ((x + col) % 64);
-						if (gfx[gfx_index] == 1) {
-							V[0xF] = 1; //COLLISION FLAG IS SET TO TRUE
+						if (gfx[gfx_index] == 1)
+						{
+							V[0xF] = 1; // COLLISION FLAG IS SET TO TRUE
 						}
-						gfx[gfx_index] ^= 1; //XOR OPERATOR
+						gfx[gfx_index] ^= 1; // XOR OPERATOR
 					}
 				}
 			}
 
-			std::cout << "DRAW FLAG" << std::endl;
+			std::cout << "------------------+DRAW FLAG" << std::endl;
 
 			draw_flag = true; // SIGNAL TO REDRAW THE SCREEN
 			break;
 		}
-		case(0xE):
+		case (0xE):
 		{
 			uint8_t nibbles = (opcode & 0xFF);
 			uint8_t x = (opcode >> 8) & 0x0F; // EXTRACT REGISTER NUMBER
 
-
-			if (nibbles == 0x9E && keys[V[x]] == 1) {
-					PC += 2;
-					break;
-
+			if (nibbles == 0x9E && keys[V[x]] == 1)
+			{
+				PC += 2;
+				break;
 			}
-			if (nibbles == 0xA1 && keys[V[x]] == 0) {
-					PC += 2;
-					break;
+			if (nibbles == 0xA1 && keys[V[x]] == 0)
+			{
+				PC += 2;
+				break;
 			}
 
 			break;
-
-
 		}
-		case(0xF):{
+		case (0xF):
+		{
 			uint8_t nibbles = (opcode & 0xFF);
 			uint8_t x = (opcode >> 8) & 0x0F; // EXTRACT REGISTER NUMBER
+			if (nibbles == 0x07) {
+				std::cout << "Delay Timer Value" << std::endl;
+				break;
+			}
+			if (nibbles == 0x15)
+			{
+				V[x] = delay_timer;
+				std::cout << "LD DT, Vx" << std::endl;
 
-			if (nibbles == 0x1E) {
+				break;
+			}
+			if (nibbles == 0x1E)
+			{
 				std::cout << "ADD I, Vx" << std::endl;
 				I = I + V[x];
 				break;
 			}
-			if (nibbles == 0x29) {
+			if (nibbles == 0x29)
+			{
 				std::cout << "LD F, Vx" << std::endl;
 				I = 5 * V[x];
 				break;
 			}
 
-			if (nibbles == 0x33) { // FX33: Store BCD representation of Vx
-				uint8_t value = V[x];          // Value in Vx
+			if (nibbles == 0x33)
+			{						  // FX33: Store BCD representation of Vx
+				uint8_t value = V[x]; // Value in Vx
 
 				// Calculate BCD digits
-				mem[I] = value / 100;       // Hundreds digit
+				mem[I] = value / 100;			 // Hundreds digit
 				mem[I + 1] = (value % 100) / 10; // Tens digit
-				mem[I + 2] = value % 10;         // Ones digit
-				
+				mem[I + 2] = value % 10;		 // Ones digit
 
-				std::cout << "============FX33========= " << std::endl;
+				std::cout << "FX33: Store BCD representation of Vx " << std::endl;
 
 				break;
 			}
-			if (nibbles == 0x55) { // FX55: 
+			if (nibbles == 0x55)
+			{ // FX55:
 
-				for (uint8_t i = 0; i <= x; i++) {
+				for (uint8_t i = 0; i <= x; i++)
+				{
 					mem[I + i] = V[i];
 				}
 
@@ -355,8 +406,10 @@ struct Chip8
 				break;
 			}
 
-			if (nibbles == 0x65) { // FX65: Read registers V[0] through V[x] from memory starting at I
-				for (uint8_t i = 0; i <= x; i++) {
+			if (nibbles == 0x65)
+			{ // FX65: Read registers V[0] through V[x] from memory starting at I
+				for (uint8_t i = 0; i <= x; i++)
+				{
 					V[i] = mem[I + i];
 				}
 
@@ -372,8 +425,10 @@ struct Chip8
 			std::cout << "UNKNOWN OPCODE: " << std::hex << opcode << " at PC: " << PC << std::endl;
 		}
 	}
-	void push(uint16_t element) {
-		if (SP > 15) { // Correct overflow condition
+	void push(uint16_t &element)
+	{
+		if (SP > 15)
+		{ // Correct overflow condition
 			std::cerr << "STACK OVERFLOW" << std::endl;
 			return;
 		}
@@ -381,21 +436,22 @@ struct Chip8
 		std::cout << "WE HAVE PUSHED " << element << " ONTO THE STACK" << std::endl;
 	}
 
-	uint16_t pop() {
-		if (SP == 0) { // Correct underflow condition
+	uint16_t pop()
+	{
+		if (SP == 0)
+		{ // Correct underflow condition
 			std::cerr << "STACK UNDERFLOW" << std::endl;
 			return 0; // Return an invalid value or handle error
 		}
 		return stack[--SP]; // Decrement SP, then retrieve value
 	}
 
-
-	uint8_t generateRandomByte() {
+	uint8_t generateRandomByte()
+	{
 		static std::random_device rd;
 		static std::mt19937 gen(rd());
 		static std::uniform_int_distribution<> dist(0, 255);
 
 		return dist(gen);
-
 	}
 };
--- main.cpp
+++ main.cpp
@@ -4,74 +4,84 @@
 #include <iomanip> // For std::hex and std::setfill
 #include <SDL.h>
 
-
 #define WIDTH 64
 #define HEIGHT 32
 #define SCALE 10
-void renderScreen(SDL_Renderer* renderer, const uint8_t* gfx) {
+void renderScreen(SDL_Renderer *renderer, const uint8_t *gfx)
+{
 
 	SDL_SetRenderDrawColor(renderer, 67, 28, 83, 2555); // SCREEN COLOR
-	SDL_RenderClear(renderer); // CLEAR THE SCREEN
+	SDL_RenderClear(renderer);							// CLEAR THE SCREEN
 
 	// SET THE DRAW COLOR
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-	for (int y = 0; y < 32; y++) {
-		for (int x = 0; x < 64; x++) {
+	for (int y = 0; y < 32; y++)
+	{
+		for (int x = 0; x < 64; x++)
+		{
 			int index = y * 64 + x;
 
-			if (gfx[index] == 1) {
-				SDL_Rect pixel = { x * SCALE, y * SCALE, SCALE, SCALE };
+			if (gfx[index] == 1)
+			{
+				SDL_Rect pixel = {x * SCALE, y * SCALE, SCALE, SCALE};
 				SDL_RenderFillRect(renderer, &pixel);
 			}
 		}
 	}
-	SDL_RenderPresent(renderer); //PRESENT THE FRAME
+	SDL_RenderPresent(renderer); // PRESENT THE FRAME
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	Chip8 chip;
 
-	const char* fileName = "corax.ch8";
+	const char *fileName = "pong.ch8";
 
 	chip.loadROM(fileName);
 
-	//LETS GET A SCREEN GOING ON
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+	// LETS GET A SCREEN GOING ON
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
 		std::cerr << "FAILED TO INITIALIZE SDL: " << SDL_GetError() << std::endl;
-		return -1; //INDICATES THAT THE FIRST VALUE IS LESS THAN THE SECOND ONE
+		return -1; // INDICATES THAT THE FIRST VALUE IS LESS THAN THE SECOND ONE
 	}
 
-	SDL_Window* window = SDL_CreateWindow("Chip-8 Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH * SCALE, HEIGHT * SCALE, SDL_WINDOW_SHOWN); //Initalizes the window
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_Window *window = SDL_CreateWindow("Chip-8 Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH * SCALE, HEIGHT * SCALE, SDL_WINDOW_SHOWN); // Initalizes the window
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	if (!window) { //Error creating window
+	if (!window)
+	{ // Error creating window
 		SDL_Quit();
 		return -1;
 	}
 
-	//PROGRAM RUNS UNTIL USER TERMINATES
+	// PROGRAM RUNS UNTIL USER TERMINATES
 	bool running = true;
 	bool paused = false;
 
 	uint32_t last_tick = SDL_GetTicks();
-	while (running) {
+	while (running)
+	{
 
-		if (!paused) {
+		if (!paused)
+		{
 			chip.emulate_Cycle();
 
-			if (chip.PC > 4096) {
+			if (chip.PC > 4096)
+			{
 				std::cerr << "PC OUT OF BOUND PAUSING CYCLE." << std::endl;
 				paused = true;
 			}
 		}
-		if (chip.draw_flag) {
+		if (chip.draw_flag)
+		{
 			renderScreen(renderer, chip.gfx);
 			chip.draw_flag = false;
 		}
 
-		if (chip.clearScreen_flag) {
+		if (chip.clearScreen_flag)
+		{
 			SDL_RenderClear(renderer);
 			chip.clearScreen_flag = false;
 		}
@@ -85,126 +95,152 @@ int main(int argc, char* argv[])
 			-------------
 			7 | 8 | 9 | E
 			-------------
-			A | 0 | B | F 
+			A | 0 | B | F
 
-		
-		
-		
-		
-		
+
+
+
+
+
 		*/
 		SDL_Event event;
-		while (SDL_PollEvent(&event)) { //Event Handling Loop
+		while (SDL_PollEvent(&event))
+		{ // Event Handling Loop
 
-			switch (event.type) {
-			case(SDL_QUIT): {
+			switch (event.type)
+			{
+			case (SDL_QUIT):
+			{
 				running = false;
 				break;
 			}
-			case(SDL_KEYDOWN): {
-				switch (event.key.keysym.sym) {
-					case(SDLK_0): {
-						std::cout << "======================= 0 KEY UNPRESSED =======================" << std::endl;
-						break;
-					}
-					case(SDLK_1): {
-						std::cout << "======================= 1 KEY PRESSED =======================" << std::endl;
-						chip.keys[1] = 1;
-						break;
-					}
-					case(SDLK_2): {
-						std::cout << "======================= 2 KEY UNPRESSED =======================" << std::endl;
-						break;
-					}
-					case(SDLK_3): {
-						std::cout << "======================= 3 KEY UNPRESSED =======================" << std::endl;
-						break;
-					}
-					case(SDLK_4): {
-						std::cout << "======================= 4 KEY UNPRESSED =======================" << std::endl;
-						break;
-					}
-					case(SDLK_5): {
-						std::cout << "======================= 5 KEY UNPRESSED =======================" << std::endl;
-						break;
-					}
-					case(SDLK_6): {
-						std::cout << "======================= 6 KEY UNPRESSED =======================" << std::endl;
-						break;
-					}
-					case(SDLK_7): {
-						std::cout << "======================= 7 KEY UNPRESSED =======================" << std::endl;
-						break;
-					}
-					case(SDLK_8): {
-						std::cout << "======================= 8 KEY UNPRESSED =======================" << std::endl;
-						break;
-					}
-					case(SDLK_9): {
-						std::cout << "======================= 9 KEY UNPRESSED =======================" << std::endl;
-						break;
-					}
-				}
-				break;
-
-			}
-			case(SDL_KEYUP): {
-				switch (event.key.keysym.sym) {
-				case(SDLK_0): {
+			case (SDL_KEYDOWN):
+			{
+				switch (event.key.keysym.sym)
+				{
+				case (SDLK_0):
+				{
+					chip.keys[0] = 1;
 					std::cout << "======================= 0 KEY UNPRESSED =======================" << std::endl;
 					break;
 				}
-				case(SDLK_1): {
+				case (SDLK_1):
+				{
 					std::cout << "======================= 1 KEY PRESSED =======================" << std::endl;
-					chip.keys[1] = 0;
+					chip.keys[1] = 1;
 					break;
 				}
-				case(SDLK_2): {
+				case (SDLK_2):
+				{
 					std::cout << "======================= 2 KEY UNPRESSED =======================" << std::endl;
+					chip.keys[2] = 1;
 					break;
 				}
-				case(SDLK_3): {
+				case (SDLK_3):
+				{
 					std::cout << "======================= 3 KEY UNPRESSED =======================" << std::endl;
 					break;
 				}
-				case(SDLK_4): {
+				case (SDLK_4):
+				{
 					std::cout << "======================= 4 KEY UNPRESSED =======================" << std::endl;
 					break;
 				}
-				case(SDLK_5): {
+				case (SDLK_5):
+				{
 					std::cout << "======================= 5 KEY UNPRESSED =======================" << std::endl;
 					break;
 				}
-				case(SDLK_6): {
+				case (SDLK_6):
+				{
 					std::cout << "======================= 6 KEY UNPRESSED =======================" << std::endl;
 					break;
 				}
-				case(SDLK_7): {
+				case (SDLK_7):
+				{
 					std::cout << "======================= 7 KEY UNPRESSED =======================" << std::endl;
 					break;
 				}
-				case(SDLK_8): {
+				case (SDLK_8):
+				{
 					std::cout << "======================= 8 KEY UNPRESSED =======================" << std::endl;
 					break;
 				}
-				case(SDLK_9): {
+				case (SDLK_9):
+				{
 					std::cout << "======================= 9 KEY UNPRESSED =======================" << std::endl;
 					break;
 				}
 				}
 				break;
-
 			}
-
+			case (SDL_KEYUP):
+			{
+				switch (event.key.keysym.sym)
+				{
+				case (SDLK_0):
+				{
+					chip.keys[0] = 0;
+					std::cout << "======================= 0 KEY UNPRESSED =======================" << std::endl;
+					break;
+				}
+				case (SDLK_1):
+				{
+					std::cout << "======================= 1 KEY PRESSED =======================" << std::endl;
+					chip.keys[1] = 0;
+					break;
+				}
+				case (SDLK_2):
+				{
+					chip.keys[2] = 0;
+					std::cout << "======================= 2 KEY UNPRESSED =======================" << std::endl;
+					break;
+				}
+				case (SDLK_3):
+				{
+					std::cout << "======================= 3 KEY UNPRESSED =======================" << std::endl;
+					break;
+				}
+				case (SDLK_4):
+				{
+					std::cout << "======================= 4 KEY UNPRESSED =======================" << std::endl;
+					break;
+				}
+				case (SDLK_5):
+				{
+					std::cout << "======================= 5 KEY UNPRESSED =======================" << std::endl;
+					break;
+				}
+				case (SDLK_6):
+				{
+					std::cout << "======================= 6 KEY UNPRESSED =======================" << std::endl;
+					break;
+				}
+				case (SDLK_7):
+				{
+					std::cout << "======================= 7 KEY UNPRESSED =======================" << std::endl;
+					break;
+				}
+				case (SDLK_8):
+				{
+					std::cout << "======================= 8 KEY UNPRESSED =======================" << std::endl;
+					break;
+				}
+				case (SDLK_9):
+				{
+					std::cout << "======================= 9 KEY UNPRESSED =======================" << std::endl;
+					break;
+				}
+				}
+				break;
+			}
+			}
 		}
+		SDL_Delay(3);
 	}
-		SDL_Delay(16);
-}
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
-
-
 
 	return 0;
 }
